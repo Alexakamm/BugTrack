56e644d136d8eb23a64917387c3496653495e7d7
alexakamm <ask3tyh@virginia.edu>
2023-06-20 18:41:43 -0400

    implemented Auth0

--- KammBugTracker/Program.cs
+++ KammBugTracker/Program.cs
@@ -1,65 +1,44 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using KammBugTracker.Data;
-using Auth0.AspNetCore.Authentication;
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using KammBugTracker.Support;
+using System.Net;
 
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// // Cookie configuration for HTTP to support cookies with SameSite=None
-//builder.Services.ConfigureSameSiteNoneCookies();
-
-
-//// Cookie configuration for HTTPS
-builder.Services.Configure<CookiePolicyOptions>(options =>
- {
-     options.MinimumSameSitePolicy = SameSiteMode.None;
- });
+//To use MVC we have to explicitly declare we are using it. Doing so will prevent a System.InvalidOperationException.
+builder.Services.AddControllersWithViews();
 builder.Services.AddAuth0WebAppAuthentication(options =>
 {
     options.Domain = builder.Configuration["Auth0:Domain"];
     options.ClientId = builder.Configuration["Auth0:ClientId"];
 });
-builder.Services.AddControllersWithViews();
-
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
+builder.Services.ConfigureSameSiteNoneCookies();
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseCookiePolicy();
 
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
-
